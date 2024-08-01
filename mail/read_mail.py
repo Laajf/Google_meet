db565019baaa20a5cbba8cbf366f6084000b2bb9
Laajf <krym4rtem@yandex.ru>
2024-08-01 13:13:54 +0300

    оптимизация кода

--- mail/read_mail.py
+++ mail/read_mail.py
@@ -4,90 +4,97 @@ from utils.titel_to_string import decode_mime_words
 from utils.body_email_to_string import extract_text_from_html
 import imaplib
 
-
 # Почтовые данные
 IMAP_SERVER = 'imap.gmail.com'
 EMAIL_ACCOUNT = 'arttdydnik@gmail.com'
 PASSWORD = 'fyax omnj uobc fwve'
 
 
+def connect_to_mail_server():
+    """Подключение к почтовому серверу и аутентификация."""
+    mail = imaplib.IMAP4_SSL(IMAP_SERVER)
+    mail.login(EMAIL_ACCOUNT, PASSWORD)
+    mail.select('inbox')
+    return mail
+
+
+def fetch_unseen_emails(mail):
+    """Поиск непрочитанных писем и возвращение их ID."""
+    status, messages = mail.search(None, '(UNSEEN)')
+    return messages[0].split()
+
+
+def process_email(mail_id, mail):
+    """Обработка отдельного письма по его ID."""
+    status, msg_data = mail.fetch(mail_id, '(RFC822)')
+    for response_part in msg_data:
+        if isinstance(response_part, tuple):
+            msg = email.message_from_bytes(response_part[1])
+            subject = decode_header_field(msg['Subject'])
+            from_ = decode_header_field(msg['From'])
+            print('Subject:', subject)
+            print('From:', from_)
+            body = extract_body_from_message(msg)
+            if body:
+                print('Body:', body)
+            else:
+                print('No text/plain or text/html body found')
+
+
+def decode_header_field(header):
+    """Декодирование заголовка с поддержкой MIME."""
+    if header:
+        return decode_mime_words(header)
+    return '(No Subject)' if header == 'Subject' else '(Unknown Sender)'
+
+
+def extract_body_from_message(msg):
+    """Извлечение тела письма из сообщения."""
+    if msg.is_multipart():
+        for part in msg.walk():
+            content_type = part.get_content_type()
+            content_disposition = str(part.get('Content-Disposition'))
+            try:
+                part_body = part.get_payload(decode=True).decode()
+            except:
+                part_body = '(Unable to decode body)'
+            if content_type == 'text/plain' and 'attachment' not in content_disposition:
+                return part_body
+            elif content_type == 'text/html' and 'attachment' not in content_disposition:
+                return extract_text_from_html(part_body)
+    else:
+        content_type = msg.get_content_type()
+        try:
+            body = msg.get_payload(decode=True).decode()
+        except:
+            body = '(Unable to decode body)'
+        if content_type == 'text/html':
+            return extract_text_from_html(body)
+        return body
+    return None
+
+
+def close_connection(mail):
+    """Закрытие соединения с почтовым сервером."""
+    mail.close()
+    mail.logout()
+
+
 def check_mail():
+    """Основная функция для проверки почты."""
     try:
-
-        mail = imaplib.IMAP4_SSL(IMAP_SERVER)
-        mail.login(EMAIL_ACCOUNT, PASSWORD)
-        mail.select('inbox')
-
-        # Поиск непрочитанных писем
-        status, messages = mail.search(None, '(UNSEEN)')
-
-        mail_ids = messages[0].split()
+        mail = connect_to_mail_server()
+        mail_ids = fetch_unseen_emails(mail)
         for mail_id in mail_ids:
-            status, msg_data = mail.fetch(mail_id, '(RFC822)')
-            for response_part in msg_data:
-                if isinstance(response_part, tuple):
-                    msg = email.message_from_bytes(response_part[1])
-
-                    # Обработка заголовка Subject
-                    subject_header = msg['Subject']
-                    if subject_header:
-                        subject = decode_mime_words(subject_header)
-                    else:
-                        subject = '(No Subject)'
-
-                    # Обработка заголовка From
-                    from_header = msg['From']
-                    if from_header:
-                        from_ = decode_mime_words(from_header)
-                    else:
-                        from_ = '(Unknown Sender)'
-
-                    print('Subject:', subject)
-                    print('From:', from_)
-
-                    # Инициализация переменной для хранения тела письма
-                    body = None
-
-                    if msg.is_multipart():
-                        for part in msg.walk():
-                            content_type = part.get_content_type()
-                            content_disposition = str(part.get('Content-Disposition'))
-
-                            try:
-                                part_body = part.get_payload(decode=True).decode()
-                            except:
-                                part_body = '(Unable to decode body)'
-
-                            if content_type == 'text/plain' and 'attachment' not in content_disposition:
-                                body = part_body
-                                break  # Остановиться после нахождения текстовой части
-                            elif content_type == 'text/html' and 'attachment' not in content_disposition:
-                                # Извлечь текст из HTML
-                                body = extract_text_from_html(part_body)
-                                break  # Остановиться после нахождения HTML части
-                    else:
-                        content_type = msg.get_content_type()
-                        try:
-                            body = msg.get_payload(decode=True).decode()
-                        except:
-                            body = '(Unable to decode body)'
-                        if content_type == 'text/html':
-                            # Извлечь текст из HTML
-                            body = extract_text_from_html(body)
-
-                    if body:
-                        print('Body:', body)
-                    else:
-                        print('No text/plain or text/html body found')
-
-        mail.close()
-        mail.logout()
+            process_email(mail_id, mail)
+        close_connection(mail)
     except Exception as e:
         print(f'Error: {e}')
 
 
-if __name__ == "__main__":
-    print("Запуск проверки почты")
+# Запуск функции проверки почты
+if __name__ == '__main__':
+    print("Запуск сервера")
     while True:
         check_mail()
-        time.sleep(1)  # Проверять почту каждые 60 секунд
+        time.sleep(1)
