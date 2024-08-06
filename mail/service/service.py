6d1efd941aa9bafbc83883e26849cab6e6a16354
Laajf <krym4rtem@yandex.ru>
2024-08-06 10:50:33 +0300

    T

--- mail/service/service.py
+++ mail/service/service.py
@@ -1,10 +1,10 @@
 from ..utils.connect_and_find_mails import connect_to_mail_server, fetch_unseen_emails
 from ..utils.treatment_mail import process_email, close_connection
-
+from for_user.utils.json_to_variable import get_config_value
 # Почтовые данные
-IMAP_SERVER = 'imap.gmail.com'
-EMAIL_ACCOUNT = 'arttdydnik@gmail.com'
-PASSWORD = 'fyax omnj uobc fwve'
+IMAP_SERVER = get_config_value("IMAP_SERVER")
+EMAIL_ACCOUNT = get_config_value("EMAIL_ACCOUNT")
+PASSWORD = get_config_value("PASSWORD")
 
 
 def check_mail():
